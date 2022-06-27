3830748357ceced6a23356496a4ac632143c93e8
teamKweku <teamkweku@outlook.com>
2022-06-27 01:04:56 -0500

    Changing default required fields value to False

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -17,3 +17,9 @@ class EmployeeForm(ModelForm):
             'fullname': 'Full Name',
             'emp_code': 'EMP. Code'
         }
+
+    # Code to change the empty label text to SELECT
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = 'Select'
+        self.fields['emp_code'].required = False
